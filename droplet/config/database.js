5c0891e1cd6f5e2d2c664c996d6e03efcc5d5baa
harixraza <106744750+harixraza@users.noreply.github.com>
2023-03-13 16:45:34 +0500

    root password added

--- droplet/config/database.js
+++ droplet/config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'db'),
-        user: env('DATABASE_USERNAME', 'roots'),
-        password: env('DATABASE_PASSWORD', ''),
+        user: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'admin123!@#AA'),
         ssl: {
           rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
        },
