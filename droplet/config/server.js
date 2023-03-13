f3ddc4c34893de3924b0f0a501470fd6e4f773ff
harixraza <106744750+harixraza@users.noreply.github.com>
2023-03-13 16:49:15 +0500

    app keys added

--- droplet/config/server.js
+++ droplet/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['D7O0HYAUvX735d6xdaXoZg==','qhYE4krxyqO02DZR4ezFQw==','6RAPs9lhIalhH3pHUuPzxQ==','eOluBzKIkTwFwmLnS7oMZA=='],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
