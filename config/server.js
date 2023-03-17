f0f85e898cc5cd571ceb586db55712ec7a6f22c7
harixraza <106744750+harixraza@users.noreply.github.com>
2023-03-17 17:45:52 +0500

    port added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  // port: 1338,
-  port: env.int('PORT', 1337),
+  port: 80,
+  // port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://teamzeros.com'),
   app: {
     keys: ['D7O0HYAUvX735d6xdaXoZg==','qhYE4krxyqO02DZR4ezFQw==','6RAPs9lhIalhH3pHUuPzxQ==','eOluBzKIkTwFwmLnS7oMZA=='],
