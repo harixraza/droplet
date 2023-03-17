3dabac1d39e91561ffe2b28cbc6730f11c6a6b00
harixraza <106744750+harixraza@users.noreply.github.com>
2023-03-17 16:17:37 +0500

    ssl fixed

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: 1338,
-  // port: env.int('PORT', 1337),
-  url: 'https://teamzeros.com',
+  // port: 1338,
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://teamzeros.com'),
   app: {
     keys: ['D7O0HYAUvX735d6xdaXoZg==','qhYE4krxyqO02DZR4ezFQw==','6RAPs9lhIalhH3pHUuPzxQ==','eOluBzKIkTwFwmLnS7oMZA=='],
   },
